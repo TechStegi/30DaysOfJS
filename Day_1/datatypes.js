f7baa2662e270f84da1f0604e7b335b98b634b06
TechStegi <sirstegi@gmail.com>
2023-10-25 15:00:28 +0200

    Add day 1

---
+++ Day_1/datatypes.js
@@ -0,0 +1,37 @@
+
+
+    
+import variable from './variable.js'
+
+
+console.log(typeof variable.testBool)
+console.log(typeof variable.place)
+console.log(typeof variable.emptyValue)
+console.log(typeof variable.text)
+
+
+let hello;
+let one;
+let two;
+var three;
+
+let v1 = 2;
+const v2 = "hello";
+let bool3 = true;
+const easy = null;
+
+const myPerson = {
+    firstName: "Sir",
+    lastName: "Stegi",
+    maritalStatus: "single",
+    country: "Germany",
+    age: 18
+}
+
+let firstName = "Sir", lastName = "Stegi", maritalStatus ="single", country="Germany", age=18;
+
+
+var myAge = 18;
+let yourAge = 20;
+
+console.log("I am",myAge,"years old.", "You are",yourAge,"years old.")
---
+++ Day_1/variable.js
@@ -0,0 +1,9 @@
+
+const testBool = false;
+let place = undefined;
+var emptyValue = null;
+var text = "hello Jim";
+
+export default {
+    testBool, place, emptyValue, text
+}
