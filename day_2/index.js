6b1bdb456937be97ab6224eb47e1d889b446c9ed
FedoraStegi <sirstegi@gmail.com>
2023-10-25 16:00:37 +0200

    Add day 2 from Laptop

---
+++ day_2/index.js
@@ -0,0 +1,17 @@
+
+let challenge = '30 Days Of JavaScript';
+
+console.log(challenge);
+console.log(challenge.length);
+console.log(challenge.toUpperCase());
+console.log(challenge.toLowerCase());
+console.log(challenge.substring(3, 8));
+console.log(challenge.slice(3));
+console.log(challenge.includes('Script'));
+console.log(challenge.split());
+console.log(challenge.split(' '));
+
+let companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+
+console.log(companies.split(','));
+
